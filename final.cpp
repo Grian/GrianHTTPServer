36314d8e86617b77fc41705b7e600ed8cf4996cb
Anatoliy Grishayev <grian@cpan.org>
2016-02-19 02:35:25 +0400

    1

--- final.cpp
+++ final.cpp
@@ -68,6 +68,7 @@ int max(int a, int b){ return a > b ? a :b; }
 int check_header(const char *msg, int read, int *url_start, int *url_end){
     int start = 0;
     int method = 0;
+    makelog("check_header", 0);
     makelog(msg, read);
     if (msg[0] == 'G' && msg[1] == 'E' && msg[2] == 'T' && msg[3] == ' ' && msg[4] == '/'){
         method = 1;
